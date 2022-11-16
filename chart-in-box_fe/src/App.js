b255898068ba59c51eae5fb285462000e094aab7
89882 <pgh123166@naver.com>
2022-11-16 16:33:34 +0900

    fix : 모달창 구현

--- chart-in-box_fe/src/App.js
+++ chart-in-box_fe/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Join from './pages/sign/Join';
-import Login from './pages/sign/Login';
-import ChangePassword from './pages/sign/ChangePassword';
+import MainPage from './pages/main/MainPage';
+import Terms from './pages/agree/Terms';
+import PrivatePolicy from './pages/agree/PrivatePolicy';
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/join" element={<Join></Join>} />
-                    <Route path="/log-in" element={<Login />} />
-                    <Route
-                        path="/log-in/find-pw"
-                        element={<ChangePassword />}
-                    />
+                    <Route path="/" element={<MainPage></MainPage>} />
+                    <Route path="/terms" element={<Terms></Terms>} />
+                    <Route path="/privatepolicy" element={<PrivatePolicy />} />
                 </Routes>
             </Router>
         </div>
---
+++ chart-in-box_fe/src/pages/main/MainPage.jsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Join from '../../components/sign/Join';
+import Login from '../../components/sign/Login';
+import ChangePassword from '../../components/sign/ChangePassword';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+
+function MainPage() {
+    const [joinOpen, setJoinOpen] = React.useState(false);
+    const handleJoinOpen = () => setJoinOpen(true);
+    const handleJoinClose = () => setJoinOpen(false);
+
+    const [loginOpen, setLoginOpen] = React.useState(false);
+    const handleLoginOpen = () => setLoginOpen(true);
+    const handleLoginClose = () => setLoginOpen(false);
+
+    const [changePasswordOpen, setChangePasswordOpen] = React.useState(false);
+    const handleChangePasswordOpen = () => setChangePasswordOpen(true);
+    const handleChangePasswordClose = () => setChangePasswordOpen(false);
+
+    const clickLogin = () => {
+        setLoginOpen(true);
+        setJoinOpen(false);
+    };
+    const clickJoin = () => {
+        setLoginOpen(false);
+        setJoinOpen(true);
+    };
+    const clickPassword = () => {
+        setLoginOpen(false);
+        setChangePasswordOpen(true);
+    };
+    return (
+        <div>
+            <Button onClick={handleJoinOpen}>회원가입</Button>
+            <Modal open={joinOpen} onClose={handleJoinClose}>
+                <Box sx={style}>
+                    <Join clickLogin={clickLogin} />
+                </Box>
+            </Modal>
+            <Button onClick={handleLoginOpen}>로그인</Button>
+            <Modal open={loginOpen} onClose={handleLoginClose}>
+                <Box sx={style}>
+                    <Login
+                        clickJoin={clickJoin}
+                        clickPassword={clickPassword}
+                    />
+                </Box>
+            </Modal>
+            <Button onClick={handleChangePasswordOpen}>비밀번호 찾기</Button>
+            <Modal
+                open={changePasswordOpen}
+                onClose={handleChangePasswordClose}
+            >
+                <Box sx={style}>
+                    <ChangePassword></ChangePassword>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
+export default MainPage;
