ea97d50a7643537d06de206e4ded4159313c1f32
89882 <pgh123166@naver.com>
2022-11-19 03:51:47 +0900

    feat: 영화정보페이지 메뉴바 구현

--- chart-in-box_fe/src/App.js
+++ chart-in-box_fe/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/main/MainPage';
 import Terms from './pages/agree/Terms';
 import PrivatePolicy from './pages/agree/PrivatePolicy';
 import Mypage from './pages/mypage/Mypage';
+import MovieInfo from './pages/movieinfo/MovieInfo';
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
-
-                    <Route path="/" element={<MainPage></MainPage>} />
                     <Route path="/terms" element={<Terms></Terms>} />
                     <Route path="/privatepolicy" element={<PrivatePolicy />} />
                     <Route path="/mypage" element={<Mypage />} />
+                    <Route
+                        path="/movieinfo"
+                        element={<MovieInfo></MovieInfo>}
+                    />
+                    <Route path="/mypage" element={<Mypage></Mypage>} />
                 </Routes>
             </Router>
         </div>
