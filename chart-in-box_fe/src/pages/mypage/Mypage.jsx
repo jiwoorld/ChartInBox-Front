728c155661f15b7591e0e7c4e944b1dfde02b4ff
jiwoorld <bncmdbev@gmail.com>
2022-11-23 14:37:53 +0900

    modify mypage

--- chart-in-box_fe/src/pages/mypage/Mypage.jsx
+++ chart-in-box_fe/src/pages/mypage/Mypage.jsx
@@ -41,91 +41,109 @@ export default function Album() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            {/* <AppBar position="relative" sx={{ border: '1px solid red' }}> */}
             <MenuBar></MenuBar>
-            {/* </AppBar> */}
             <main>
-                {/* Hero unit */}
                 <Box
                     sx={{
                         bgcolor: 'background.paper',
-                        pt: 8,
-                        pb: 6,
-                        paddingLeft: '25%',
+                        px: 32,
+                        pb: 10,
+                        mt: 10,
+                        direction: 'column',
+                        align: 'center',
+                        justifyContent: 'space-around',
                         borderBottom: 1,
                         borderBottomColor: 'grey.500',
                     }}
                 >
-                    <Container maxWidth="550px">
-                        <Typography
-                            component="h1"
-                            //variant="h2"
-                            align="left"
-                            height="50px"
-                            color="text.primary"
-                            fontSize="1.3125 rem"
-                            fontWeight="1500px"
-                            maxWidth="200px"
-                            gutterBottom
-                        >
-                            내 프로필
-                            <CardActions>
-                                <Button size="medium">개인정보수정</Button>
-                            </CardActions>
-                        </Typography>
-
-                        <Stack
-                            sx={{ pt: 4 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <CardContent sx={{ flexGrow: 1 }}>
+                    {/* <Container maxWidth="550px"> */}
+                    <Button
+                        sx={{
+                            align: 'left',
+                            fontFamily: 'Pretendard',
+                            fontWeight: '600',
+                            fontSize: '21px',
+                            lineHeight: '25px',
+                            color: 'text.primary',
+                            ml: '-30em',
+                        }}
+                    >
+                        내프로필
+                    </Button>
+                    <Button
+                        sx={{
+                            width: '161px',
+                            height: '60px',
+                            fontFamily: 'Pretendard',
+                            fontStyle: 'normal',
+                            fontWeight: '400',
+                            fontSize: '1.125rem',
+                            color: '#757575',
+                        }}
+                    >
+                        {' '}
+                        개인정보수정{' '}
+                    </Button>
+                    <Grid container spacing={0.5}>
+                        <Grid item xs={12} sm={6}>
+                            <Box
+                                sx={{
+                                    ml: '5em',
+                                    mt: '5em',
+                                }}
+                            >
                                 <Typography
                                     gutterBottom
-                                    variant="h5"
-                                    fontSize="1.125"
+                                    fontSize="1.125rem"
+                                    fontWeight="400"
                                     textAlign="left"
+                                    color="#757575"
                                 >
                                     닉네임
                                 </Typography>
                                 <Typography
                                     textAlign="left"
-                                    fontSize="1.75rem"
-                                    fontWeight="700"
+                                    fontSize="1.5rem"
+                                    fontWeight="600"
                                 >
                                     잇타2피
                                 </Typography>
-                            </CardContent>
-                            <CardContent sx={{ flexGrow: 1 }}>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Box sx={{ ml: '5em', mt: '5em' }}>
                                 <Typography
                                     gutterBottom
-                                    variant="h5"
-                                    fontSize="1rem"
+                                    fontSize="1.125rem"
+                                    fontWeight="400"
                                     textAlign="left"
+                                    color="#757575"
                                 >
                                     이메일
                                 </Typography>
                                 <Typography
                                     textAlign="left"
-                                    fontSize="1.75rem"
-                                    fontWeight="700"
+                                    fontSize="1.5rem"
+                                    fontWeight="600"
                                 >
                                     chartinbox@gmail.com
                                 </Typography>
-                            </CardContent>
-                        </Stack>
-                    </Container>
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Box>
-                <Container sx={{ py: 10 }} paddingLeft="20%">
-                    <Box sx={{ padding: '10px', leftMargin: '30px' }}>
-                        <Grid container spacing={2}>
+
+                <Container
+                    sx={{ py: 10, leftMargin: '200%' }}
+                    paddingLeft="20%"
+                >
+                    <Box sx={{ p: '10px', ml: '10%' }}>
+                        <Grid container spacing={4} sx={{ ml: '2em' }}>
                             <Grid
                                 card
-                                xs={3.5}
+                                xs={5}
                                 sx={{
                                     border: 1,
-                                    margin: '1em',
                                     borderColor: 'white',
                                 }}
                             >
@@ -133,13 +151,11 @@ export default function Album() {
                                     <Box sx={{ paddingBottom: '30px' }}>
                                         <Typography
                                             gutterBottom
-                                            variant="h5"
-                                            component="h2"
                                             textAlign="left"
-                                            fontSize="1.5rem"
-                                            fontWeight="530"
+                                            fontSize="1.313rem"
+                                            fontWeight="400"
                                         >
-                                            내 글
+                                            내 글 >
                                         </Typography>
                                     </Box>
                                     <Box
@@ -196,28 +212,34 @@ export default function Album() {
                                     </Box>
                                 </CardContent>
                                 <CardActions>
-                                    <Button size="medium">See more</Button>
+                                    <Button
+                                        size="medium"
+                                        sx={{
+                                            color: '#CF5E53',
+                                        }}
+                                    >
+                                        See more
+                                    </Button>
                                 </CardActions>
                             </Grid>
+
                             <Grid
                                 card
-                                xs={3.5}
+                                xs={5}
                                 sx={{
                                     border: 1,
-                                    margin: '1em',
                                     borderColor: 'white',
                                 }}
                             >
-                                {/*<card>xs=4</card>*/}
                                 <CardContent sx={{ flexGrow: 1 }}>
                                     <Box sx={{ paddingBottom: '30px' }}>
                                         <Typography
                                             gutterBottom
-                                            variant="h5"
-                                            component="h2"
                                             textAlign="left"
+                                            fontSize="1.313rem"
+                                            fontWeight="400"
                                         >
-                                            내 댓글
+                                            내 글 >
                                         </Typography>
                                     </Box>
                                     <Box
@@ -227,8 +249,8 @@ export default function Album() {
                                         }}
                                     >
                                         <Typography
-                                            margin="20px"
                                             textAlign="left"
+                                            margin="20px"
                                         >
                                             각 내용이 들어갈 칸!
                                         </Typography>
@@ -240,8 +262,8 @@ export default function Album() {
                                         }}
                                     >
                                         <Typography
-                                            margin="20px"
                                             textAlign="left"
+                                            margin="20px"
                                         >
                                             각 내용이 들어갈 칸!
                                         </Typography>
@@ -274,92 +296,21 @@ export default function Album() {
                                     </Box>
                                 </CardContent>
                                 <CardActions>
-                                    <Button size="medium">See more</Button>
-                                </CardActions>
-                            </Grid>
-                            <Grid
-                                card
-                                xs={3.5}
-                                sx={{
-                                    border: 1,
-                                    margin: '1em',
-                                    borderColor: 'white',
-                                }}
-                            >
-                                {/*<card>xs=4</card>*/}
-                                <CardContent sx={{ flexGrow: 1 }}>
-                                    <Box sx={{ paddingBottom: '30px' }}>
-                                        <Typography
-                                            gutterBottom
-                                            variant="h5"
-                                            component="h2"
-                                            textAlign="left"
-                                        >
-                                            마음함
-                                        </Typography>
-                                    </Box>
-                                    <Box
+                                    <Button
+                                        size="medium"
                                         sx={{
-                                            borderBottom: 1,
-                                            borderColor: 'grey.500',
+                                            color: '#CF5E53',
                                         }}
                                     >
-                                        <Typography
-                                            margin="20px"
-                                            textAlign="left"
-                                        >
-                                            각 내용이 들어갈 칸!
-                                        </Typography>
-                                    </Box>
-                                    <Box
-                                        sx={{
-                                            borderBottom: 1,
-                                            borderColor: 'grey.500',
-                                        }}
-                                    >
-                                        <Typography
-                                            margin="20px"
-                                            textAlign="left"
-                                        >
-                                            각 내용이 들어갈 칸!
-                                        </Typography>
-                                    </Box>
-                                    <Box
-                                        sx={{
-                                            borderBottom: 1,
-                                            borderColor: 'grey.500',
-                                        }}
-                                    >
-                                        <Typography
-                                            margin="20px"
-                                            textAlign="left"
-                                        >
-                                            각 내용이 들어갈 칸!
-                                        </Typography>
-                                    </Box>
-                                    <Box
-                                        sx={{
-                                            borderBottom: 1,
-                                            borderColor: 'grey.500',
-                                        }}
-                                    >
-                                        <Typography
-                                            margin="20px"
-                                            textAlign="left"
-                                        >
-                                            각 내용이 들어갈 칸!
-                                        </Typography>
-                                    </Box>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="medium">See more</Button>
+                                        See more
+                                    </Button>
                                 </CardActions>
                             </Grid>
                         </Grid>
                     </Box>
                 </Container>
             </main>
-            {/* Footer */}
+
             <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
                 <Typography variant="h6" align="center" gutterBottom>
                     Footer
