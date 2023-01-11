dcbd5ecd428f4ff1f463fed05d223919e38006eb
jiwoorld <bncmdbev@gmail.com>
2023-01-12 00:32:35 +0900

    writing css

--- chart-in-box_fe/src/pages/Writing/Writing.jsx
+++ chart-in-box_fe/src/pages/Writing/Writing.jsx
@@ -22,11 +22,16 @@ import MenuBar from '../../components/menubar/MenuBar';
 import { Paper } from '@mui/material';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { styled } from '@mui/material/styles';
 import { borderBottom } from '@mui/system';
 import { pink } from '@mui/material/colors';
 import Checkbox from '@mui/material/Checkbox';
 import Input from '@mui/material/Input';
 import TextField from '@mui/material/TextField';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 //import ReactHtmlParser from 'react-html-parser';
 
 function Writing() {
@@ -54,6 +59,21 @@ function Writing() {
     const handleBoardChange = event => {
         setBoard(event.target.value);
     };
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+    const a = (
+        <img
+            width="12px"
+            height="11px"
+            src="../../image/good.png"
+            alt="좋아요"
+        ></img>
+    );
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
@@ -95,103 +115,168 @@ function Writing() {
                     <Button variant="outlined">등록</Button>
                 </Box>
                 <Container
-                    Width="76.125rem"
                     sx={{
-                        alignItems: 'center',
+                        maxWidth: '85.125rem',
                         display: 'flex',
-                        flexDirection: 'row',
+                        alignItems: 'center',
+                        margin: 'auto',
+                        pt: 3,
                     }}
                 >
-                    <Box
-                        component="form"
-                        sx={{
-                            '& > :not(style)': {
-                                pb: 2,
-                                width: '53.5rem',
-                                mt: 3,
-                                ml: -2,
-                                //flexDirection: 'row',
-                            },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <TextField
-                            id="outlined-basic"
-                            label="제목을 입력해 주세요."
-                            variant="outlined"
-                        />
-                    </Box>
-
-                    <Box
-                        sx={{
-                            Height: '3rem',
-                            alignItems: 'flex-start',
-                            ml: 3,
-                            pb: 2,
-                            mt: 3,
-                        }}
-                    >
-                        <FormControl sx={{ m: 1, width: 300 }}>
-                            <Select
-                                displayEmpty
-                                inputProps={{
-                                    'aria-label': 'Without label',
+                    <Grid container spacing={2}>
+                        <Grid item xs={9}>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& > :not(style)': {
+                                        /* pb: 2, */
+                                        width: '53.5rem',
+                                        /* mt: 3,*/
+                                        // ml: -4,
+                                    },
                                 }}
-                                value={board}
-                                label="Board"
-                                onChange={handleBoardChange}
+                                noValidate
+                                autoComplete="off"
                             >
-                                <MenuItem value="">리뷰게시판</MenuItem>
-                                <MenuItem value={10}>자유게시판</MenuItem>
-                                <MenuItem value={20}>Q&A</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Box>
-                </Container>
-                <Container
-                    Width="76.125rem"
-                    sx={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        //margin: 'auto',
-                    }}
-                >
-                    <div
-                        className="App"
-                        sx={{
-                            '& > :not(style)': {
-                                pb: 2,
-                                width: '53.5rem',
-                                mt: 3,
-                                ml: -2,
-                            },
-                        }}
-                    >
-                        <CKEditor
-                            Width="76.125rem"
-                            editor={ClassicEditor}
-                            config={{
-                                placeholder: '내용을 입력하세요.',
-                            }}
-                            onReady={editor => {
-                                // You can store the "editor" and use when it is needed.
-                                console.log('Editor is ready to use!', editor);
-                            }}
-                            onChange={(event, editor) => {
-                                const data = editor.getData();
-                                console.log({ event, editor, data });
-                            }}
-                            onBlur={(event, editor) => {
-                                console.log('Blur.', editor);
-                            }}
-                            onFocus={(event, editor) => {
-                                console.log('Focus.', editor);
-                            }}
-                        />
-                    </div>
-                    <Box>df</Box>
+                                <TextField
+                                    id="outlined-basic"
+                                    label="제목을 입력해 주세요."
+                                    variant="outlined"
+                                />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Box
+                                sx={
+                                    {
+                                        //Height: '3rem',
+                                        //alignItems: 'flex-start',
+                                        /* ml: 3,
+                                    pb: 2,
+                                    mt: 3, */
+                                    }
+                                }
+                            >
+                                <FormControl sx={{ width: 300 }}>
+                                    <Select
+                                        displayEmpty
+                                        inputProps={{
+                                            'aria-label': 'Without label',
+                                        }}
+                                        value={board}
+                                        label="Board"
+                                        onChange={handleBoardChange}
+                                    >
+                                        <MenuItem value="">리뷰게시판</MenuItem>
+                                        <MenuItem value={10}>
+                                            자유게시판
+                                        </MenuItem>
+                                        <MenuItem value={20}>Q&A</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={9}>
+                            <Box
+                                className="App"
+                                sx={{
+                                    '& > :not(style)': {
+                                        /* pb: 2, */
+                                        maxWidth: '53.5rem',
+                                        /* mt: 3, */
+                                        //ml: -21,
+                                    },
+                                }}
+                            >
+                                <CKEditor
+                                    height="30rem"
+                                    editor={ClassicEditor}
+                                    config={{
+                                        placeholder: '내용을 입력하세요.',
+                                    }}
+                                    onReady={editor => {
+                                        // You can store the "editor" and use when it is needed.
+                                        console.log(
+                                            'Editor is ready to use!',
+                                            editor,
+                                        );
+                                    }}
+                                    onChange={(event, editor) => {
+                                        const data = editor.getData();
+                                        console.log({ event, editor, data });
+                                    }}
+                                    onBlur={(event, editor) => {
+                                        console.log('Blur.', editor);
+                                    }}
+                                    onFocus={(event, editor) => {
+                                        console.log('Focus.', editor);
+                                    }}
+                                />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={3}>
+                            {/*  <Box sx={{ border: 1 }}> */}
+                            <Box
+                                sx={{
+                                    border: 1,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-end',
+                                    width: '18.8rem',
+                                    p: '20px 15px 40px 7px',
+                                    gap: '8px',
+                                    border: 0,
+                                    textAlign: 'left',
+                                    borderRadius: '0.25rem',
+                                    backgroundColor: '#F5F5F5',
+                                }}
+                            >
+                                • 제목에는 영화 스포를 지양해주세요. . . . . •
+                                타인에게 불쾌감을 주는 욕설, 성적인 발언 등이
+                                포함된 글은 추후 운영진에 의해 삭제 조치될 수
+                                있습니다.
+                            </Box>
+                            <Button
+                                align="left"
+                                variant="contained"
+                                endIcon={<ThumbUpAltIcon />}
+                                sx={{
+                                    backgroundColor: '#F2CB05',
+                                    ml: -9,
+                                    mt: 2.5,
+                                }}
+                            >
+                                이 영화를 추천합니다.
+                            </Button>
+                            <Button
+                                variant="contained"
+                                endIcon={<ThumbDownAltIcon />}
+                                sx={{
+                                    backgroundColor: '#CF5E53',
+                                    ml: -4.2,
+                                    mt: 1,
+                                }}
+                            >
+                                이 영화를 추천하지 않습니다.
+                            </Button>
+                            {/* <Button
+                                variant="contained"
+                                endIcon={<ThumbUpAltIcon />}
+                                sx={{ backgroundColor: '#F2CB05' }}
+                            >
+                                이 영화를 추천합니다.
+                            </Button>
+                            <Button
+                                variant="contained"
+                                endIcon={<ThumbDownAltIcon />}
+                                sx={{ backgroundColor: '#CF5E53' }}
+                            >
+                                이 영화를 추천하지 않습니다.
+                            </Button> */}
+                            {/* </Box> */}
+                        </Grid>
+                    </Grid>
                 </Container>
             </main>
 
